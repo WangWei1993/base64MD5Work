ba1b5c9067aac71caa132bd2486e44bee46d2a99
wangwei <wei.wang@dragonsoftbravo.com>
2017-01-10 13:23:13 +0800

    第二次提交
    C
    
    git push

--- 加密/加密/AppDelegate.m
+++ 加密/加密/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     NSLog(@"呵呵");
     return YES;
 }
---
+++ 加密/加密/NSString+MD5.m
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5.m
+//  加密
+//
+//  Created by 王伟 on 2017/1/10.
+//  Copyright © 2017年 王伟. All rights reserved.
+//
+
+#import "NSString+MD5.h"
+
+@implementation NSString (MD5)
+
+@end
