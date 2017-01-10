bd27de9d5744d38d196704e1e0dd709d77533c57
wangwei <wei.wang@dragonsoftbravo.com>
2017-01-10 13:30:01 +0800

    加密64位

---
+++ 加密/加密/NSString+Base64.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Base64.h
+//  加密
+//
+//  Created by 王伟 on 2017/1/10.
+//  Copyright © 2017年 王伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Base64)
+
+@end
---
+++ 加密/加密/NSString+Base64.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Base64.m
+//  加密
+//
+//  Created by 王伟 on 2017/1/10.
+//  Copyright © 2017年 王伟. All rights reserved.
+//
+
+#import "NSString+Base64.h"
+
+@implementation NSString (Base64)
+
+@end
